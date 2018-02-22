26512aa5ddb3c7cfaf3f00f77ee8acd2eb932ee4
Egor Artemov <egor.artemov@gmail.com>
2018-02-22 04:44:00 +0300

    Extract function main() into separate module
    Extracting `main()` function allows to test everything except `main()`
    function. This patch also builds applications source files to static
    convenience library and then link it to entry and unit-tests executable
    without recompilation. Building static library adds requirements for
    configuration - now the user must have `ranlib` tool.

--- tests/topdax_suite.c
+++ tests/topdax_suite.c
@@ -8,12 +8,22 @@
 
 #include <check.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include "topdax/topdax.h"
 
-START_TEST(test_first)
+START_TEST(test_verbose_flag)
 {
-	if (0) {
-		ck_abort_msg("test that fails");
-	}
+	char *argv[] = { "./topdax", "--verbose", NULL };
+	ck_assert_int_eq(topdax_run(2, &argv[0]), EXIT_SUCCESS);
+}
+END_TEST
+
+START_TEST(test_unsupported_flag)
+{
+	char *argv[] = { "./topdax", "--unsupported", NULL };
+	/* Do not print anything on assert */
+	close(2);
+	topdax_run(2, &argv[0]);
 }
 END_TEST
 
@@ -22,7 +32,8 @@ int main(void)
 	Suite *suite = suite_create("topdax");
 
 	TCase *tc_core = tcase_create("Core");
-	tcase_add_test(tc_core, test_first);
+	tcase_add_exit_test(tc_core, test_unsupported_flag, 64);
+	tcase_add_test(tc_core, test_verbose_flag);
 	suite_add_tcase(suite, tc_core);
 
 	SRunner *sr = srunner_create(suite);
