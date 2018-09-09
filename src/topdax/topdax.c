45cdfcab48fd8be8e4d7baeee7a7269e0fd09dc7
Egor Artemov <egor.artemov@gmail.com>
2018-09-09 23:48:08 +0300

    Enable instance extensions required for WSI integration
    GLFW returns list of required extensions that depends on windows system
    API used in back-end.

--- src/topdax/topdax.c
+++ src/topdax/topdax.c
@@ -21,19 +21,42 @@ const char *const application_bug_address = PACKAGE_BUGREPORT;
 const char *const application_description =
     "The program that renders triangle using Vulkan API";
 
-/** Topdax Vulkan Instance information */
-static const VkInstanceCreateInfo topdax_vk_info = {
-	.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO
+/** Topdax application information */
+static const VkApplicationInfo topdax_app_info = {
+	.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO,
+	.pApplicationName = PACKAGE,
+	.applicationVersion = VK_MAKE_VERSION(0, 3, 0),
+	.pEngineName = PACKAGE,
+	.engineVersion = VK_MAKE_VERSION(0, 3, 0),
+	.apiVersion = VK_API_VERSION_1_1
 };
+
+static VkResult vk_instance_create(VkInstance * vk)
+{
+	uint32_t exts_count;
+	const char **exts = glfwGetRequiredInstanceExtensions(&exts_count);
+
+	const VkInstanceCreateInfo vk_info = {
+		.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO,
+		.pApplicationInfo = &topdax_app_info,
+		.enabledLayerCount = 0,
+		.ppEnabledLayerNames = NULL,
+		.enabledExtensionCount = exts_count,
+		.ppEnabledExtensionNames = exts
+	};
+	return vkCreateInstance(&vk_info, NULL, vk);
+}
 
 int application_startup(struct runloop *loop)
 {
 	if (!glfwInit())
 		return 1;
 	app.mainloop = loop;
-	if (vkCreateInstance(&topdax_vk_info, NULL, &app.vk) != VK_SUCCESS) {
+
+	if (vk_instance_create(&app.vk) != VK_SUCCESS) {
 		return 1;
 	}
+
 	topdax_window_init(&app.window, &app);
 	return 0;
 }
